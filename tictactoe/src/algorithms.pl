bb9c8c0e4adc5741721afc5b1546f6951cf55b6c
Jouke van der Maas <jouke.vandermaas@gmail.com>
2012-06-04 14:05:45 +0200

    test

---
+++ tictactoe/src/algorithms.pl
@@ -0,0 +1,62 @@
+% The alpha-beta algorithm
+
+alphabeta( Pos, Alpha, Beta, GoodPos, Val)  :-
+  moves( Pos, PosList), !,
+  boundedbest( PosList, Alpha, Beta, GoodPos, Val);
+  staticval( Pos, Val).                              % Static value of Pos 
+
+boundedbest( [Pos | PosList], Alpha, Beta, GoodPos, GoodVal)  :-
+  alphabeta( Pos, Alpha, Beta, _, Val),
+  goodenough( PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal).
+
+goodenough( [], _, _, Pos, Val, Pos, Val)  :-  !.    % No other candidate
+
+goodenough( _, Alpha, Beta, Pos, Val, Pos, Val)  :-
+  min_to_move( Pos), Val > Beta, !                   % Maximizer attained upper bound
+  ;
+  max_to_move( Pos), Val < Alpha, !.                 % Minimizer attained lower bound
+
+goodenough( PosList, Alpha, Beta, Pos, Val, GoodPos, GoodVal)  :-
+  newbounds( Alpha, Beta, Pos, Val, NewAlpha, NewBeta),    % Refine bounds  
+  boundedbest( PosList, NewAlpha, NewBeta, Pos1, Val1),
+  betterof( Pos, Val, Pos1, Val1, GoodPos, GoodVal).
+
+newbounds( Alpha, Beta, Pos, Val, Val, Beta)  :-
+  min_to_move( Pos), Val > Alpha, !.                 % Maximizer increased lower bound 
+
+newbounds( Alpha, Beta, Pos, Val, Alpha, Val)  :-
+   max_to_move( Pos), Val < Beta, !.                 % Minimizer decreased upper bound 
+
+newbounds( Alpha, Beta, _, _, Alpha, Beta).          % Otherwise bounds unchanged 
+
+betterof( Pos, Val, Pos1, Val1, Pos, Val)  :-        % Pos better than Pos1 
+  min_to_move( Pos), Val > Val1, !
+  ;
+  max_to_move( Pos), Val < Val1, !.
+
+betterof( _, _, Pos1, Val1, Pos1, Val1).             % Otherwise Pos1 better
+
+% Minimax
+
+minimax( Pos, BestSucc, Val)  :-
+  moves( Pos, PosList), !,               % Legal moves in Pos produce PosList
+  best( PosList, BestSucc, Val)
+   ;
+  staticval( Pos, Val).                 % Pos has no successors: evaluate statically 
+
+best( [ Pos], Pos, Val)  :-
+  minimax( Pos, _, Val), !.
+
+best( [Pos1 | PosList], BestPos, BestVal)  :-
+  minimax( Pos1, _, Val1),
+  best( PosList, Pos2, Val2),
+  betterof( Pos1, Val1, Pos2, Val2, BestPos, BestVal).
+
+betterof( Pos0, Val0, Pos1, Val1, Pos0, Val0)  :-        % Pos0 better than Pos1
+  min_to_move( Pos0),                                    % MIN to move in Pos0
+  Val0 > Val1, !                                         % MAX prefers the greater value
+  ;
+  max_to_move( Pos0),                                    % MAX to move in Pos0
+  Val0 < Val1, !.                                % MIN prefers the lesser value 
+
+betterof( Pos0, Val0, Pos1, Val1, Pos1, Val1).           % Otherwise Pos1 better than Pos0
---
+++ tictactoe/src/tictactoe.pl
@@ -0,0 +1,57 @@
+:- consult('algorithms.pl').
+:- consult('print.pl').
+
+% Board representation:
+% Actor/Board
+%
+% Where actor is either -1 or 1, depending on whose turn it is.
+% Board is a flat list containing 1, -1 or 0s depending on its state.
+
+% Sample game boards for testing purposes
+game_start([0,0,0, 0,0,0, 0,0,0]).
+game_tie([1,-1,1,-1,-1,1,1,1,-1]).
+game_win1([1,-1,0,0,0,-1,0,0,1]).
+game_win2([1,1,0,1,-1,-1,0,-1,0]).
+
+% You can only move if the game is not finished. Use findall
+% to find all possible moves and put them in a list.
+moves(Pos, PosList) :-
+    \+ staticval(Pos, _),
+    findall(New, possible_move(Pos, New), PosList).
+
+% Find an empty spot (there's a 0), change it to the actor
+% and make it into a new list.
+possible_move(Actor/Pos, NewActor/New) :-
+    append(Start, [0|End], Pos),
+    append(Start, [Actor|End], New),
+    NewActor is Actor * -1.
+
+% This is saved in the state, so it's easy to use.
+min_to_move(-1/_).
+max_to_move(1/_).  
+
+% Check's if there's a horizontal threesome
+hor([A, A, A|_], A) :- A \= 0.    
+hor([_, _, _| Rest], Val) :-
+	hor(Rest, Val).    
+
+% Check's if there's a vertical threesome
+ver([A, _, _, A, _, _, A, _, _], A) :- A \= 0.
+ver([_, A, _, _, A, _, _, A, _], A) :- A \= 0.
+ver([_, _, A, _, _, A, _, _, A], A) :- A \= 0.
+
+% Check's if there's a diagonal threesome
+diag([A, _, _, _, A, _, _, _, A], A) :- A \= 0.
+diag([_, _, A, _, A, _, A, _, _], A) :- A \= 0.
+
+% Check's if the game is a tie (board is full)
+tie([], 0).
+tie([H|T], 0):-
+    H \= 0,
+    tie(T, 0).
+    
+staticval(_/Pos, Val) :-
+    diag(Pos, Val);
+    hor(Pos, Val);
+    ver(Pos, Val);
+    tie(Pos, Val).
