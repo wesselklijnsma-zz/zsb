c2f6ad8ff4729db26d26e783b63d58403378dd92
Jouke van der Maas <jouke.vandermaas@gmail.com>
2012-06-05 15:04:58 +0200

    advice schema toegevoegd

---
+++ chess/KRAPpawn.pl
@@ -0,0 +1,27 @@
+% -*- Mode: Prolog -*-
+% King and Pawn vs king in Advice Language 0
+
+% all rules
+
+square_rule :: if king_in_square or king_blocked
+	then [ race_pawn ].
+else_rule :: if true
+	then [ block_king, race_pawn ].
+
+% pieces of advice
+% structure:
+% advice( NAME, BETTERGOAL, HOLDINGGOAL: USMOVECONSTRAINT: 
+%		THEMMOVECONSTRAINT
+
+
+advice( race_pawn, 
+	promotionmove :
+	not pawnlost :
+	pawnmove :
+        legal ).
+
+advice( block_king,
+	king_blocked or not king_in_square,
+	not pawnlost :
+	legal ).
+	
