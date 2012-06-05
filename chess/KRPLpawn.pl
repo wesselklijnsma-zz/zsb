35bf147923b69e4af650fe17cd76c9187e93e3fd
wesselklijnsma <klijnsma@u019936.science.uva.nl>
2012-06-05 14:52:08 +0200

    comments

--- chess/KRPLpawn.pl
+++ chess/KRPLpawn.pl
@@ -16,20 +16,20 @@ move(A,B,C,D):-
 move(pawnmove, us..W..Px : Py..B..D, Px:Py - P, them..W..P..B..D1 ) :-
 	D1 is D + 1,
 
-	P is Py + 1,
+	P is Py + 1, % moves pawn up one place
 
-	P \= Px:Py,
-	P \= W,
-	P \= B.
+	P \= Px:Py, % must move
+	P \= W, % can't move to occupied place
+	P \= B. % '' 
 
 move(promotionmove, Pos, Px:Py - P, Pos1 ) :-
- 	wp( Pos, Px:7 ),
+ 	wp( Pos, Px:7 ), % pawn must be one place away from opposite side
 
- 	P = Px:8,
+ 	P = Px:8, % moves up to the opposite side
 	move(pawnmove, Pos, Px:Py - P, Pos1).
 	
 pawnlost(_.._W..B..B.._,_).
 
 pawnlost( them..W..P..B.._,_) :-
-	ngb( B, P),
-	not ngb( W, P).   	
+	ngb( B, P), %black king attacks pawn
+	not ngb( W, P).   %white king doesn't defend	
